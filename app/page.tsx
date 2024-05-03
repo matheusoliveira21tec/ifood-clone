cc6788cc32837730c17f1043db65a03992c01710
monegasco <mhoa.2000@gmail.com>
2024-05-03 16:57:07 -0300

    feat: add component list restaurant

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import Header from "./_components/header";
 import Search from "./_components/search";
 import ProductList from "./_components/product-list";
 import { Button } from "./_components/ui/button";
-import { ChevronsRightIcon } from "lucide-react";
+import { ChevronRightIcon } from "lucide-react";
 import { db } from "./_lib/prisma";
 import PromoBanner from "./_components/promo-banner";
+import RestaurantList from "./_components/restaurant-list";
 
 const Home = async () => {
   const products = await db.product.findMany({
@@ -47,7 +48,7 @@ const Home = async () => {
             className="h-fit p-0 text-primary hover:bg-transparent"
           >
             Ver todos
-            <ChevronsRightIcon size={16} />
+            <ChevronRightIcon size={16} />
           </Button>
         </div>
         <ProductList products={products} />
@@ -57,6 +58,20 @@ const Home = async () => {
           src="/promo-banner-02.png"
           alt="A partir de R$17,90 em lanches"
         />
+      </div>
+      <div className="space-y-4 py-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Restaurantes Recomendados</h2>
+
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+            asChild
+          >
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <RestaurantList />
       </div>
     </>
   );
