13c47d37c39dbc5e84126b3014626e88847fc3ae
monegasco <mhoa.2000@gmail.com>
2024-05-02 23:20:41 -0300

    feat: add initial header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import Header from "./_components/header";
+
 export default function Home() {
-  return (
-    <div className="bg-red-500 px-4">
-      <h1>Hello TailWind</h1>
-    </div>
-  );
+  return <Header />;
 }
