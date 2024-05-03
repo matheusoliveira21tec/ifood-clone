4eeb85e16742900fd07cc4e970570c7003f1071d
monegasco <mhoa.2000@gmail.com>
2024-05-03 00:27:26 -0300

    feat: add promo banner to homepage

--- app/_components/category-item.tsx
+++ app/_components/category-item.tsx
@@ -7,7 +7,7 @@ interface CategoryItemProps {
 }
 const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
-    <div className="flex items-center gap-3 rounded-full bg-white px-4 py-3 shadow-md">
+    <div className="flex items-center justify-center gap-3 rounded-full bg-white px-4 py-3 shadow-md">
       <Image
         src={category.imageUrl}
         alt={category.name}
@@ -15,7 +15,7 @@ const CategoryItem = ({ category }: CategoryItemProps) => {
         height={30}
       />
 
-      <span className="text-sm font-semibold">{category.name}</span>
+      <span className="block text-sm font-semibold">{category.name}</span>
     </div>
   );
 };
