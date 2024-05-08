bc1173b66455efbb8d31a7d418adc3e7c6b0fd1b
monegasco <mhoa.2000@gmail.com>
2024-05-08 14:13:21 -0300

    feat: add cart and cart-item components

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import Cart from "@/app/_components/cart";
 import DeliveryInfo from "@/app/_components/delivery-info";
 import DiscountBadge from "@/app/_components/discount-badge";
 import ProductList from "@/app/_components/product-list";
 
 import { Button } from "@/app/_components/ui/button";
-
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/app/_components/ui/sheet";
+import { CartContext } from "@/app/_context/cart";
 import {
   formatCurrency,
   calculateProductTotalPrice,
@@ -13,7 +20,7 @@ import {
 import { Prisma } from "@prisma/client";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 interface ProductDetailsProps {
   product: Prisma.ProductGetPayload<{
@@ -33,7 +40,20 @@ const ProductDetails = ({
   complementaryProducts,
 }: ProductDetailsProps) => {
   const [quantity, setQuantity] = useState(1);
-  useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const { addProductToCart } = useContext(CartContext);
+
+  const addToCart = ({ emptyCart }: { emptyCart?: boolean }) => {
+    addProductToCart({ product: { ...product, quantity }, emptyCart });
+    setIsCartOpen(true);
+  };
+
+  const handleAddToCartClick = () => {
+    addToCart({
+      emptyCart: false,
+    });
+  };
 
   const handleIncreaseQuantityClick = () =>
     setQuantity((currentState) => currentState + 1);
@@ -54,6 +74,7 @@ const ProductDetails = ({
               src={product.restaurant.imageUrl}
               alt={product.restaurant.name}
               fill
+              sizes="100%"
               className="rounded-full object-cover"
             />
           </div>
@@ -102,21 +123,40 @@ const ProductDetails = ({
             </Button>
           </div>
         </div>
+
         <div className="px-5">
           <DeliveryInfo restaurant={product.restaurant} />
         </div>
+
         <div className="mt-6 space-y-3 px-5">
           <h3 className="font-semibold">Sobre</h3>
           <p className="text-sm text-muted-foreground">{product.description}</p>
         </div>
+
         <div className="mt-6 space-y-3">
           <h3 className="px-5 font-semibold">Sucos</h3>
           <ProductList products={complementaryProducts} />
         </div>
+
         <div className="mt-6 px-5">
-          <Button className="w-full font-semibold">Adicionar à sacola</Button>
+          <Button
+            className="w-full font-semibold"
+            onClick={handleAddToCartClick}
+          >
+            Adicionar à sacola
+          </Button>
         </div>
       </div>
+
+      <Sheet open={isCartOpen} onOpenChange={setIsCartOpen}>
+        <SheetContent className="w-[90vw]">
+          <SheetHeader>
+            <SheetTitle className="text-left">Sacola</SheetTitle>
+          </SheetHeader>
+
+          <Cart />
+        </SheetContent>
+      </Sheet>
     </>
   );
 };
